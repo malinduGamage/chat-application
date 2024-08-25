73a96e5de924406b5786941ac909fa39a6d71061
malinduGamage <malindug.21@cse.mrt.ac.lk>
2024-08-25 10:31:01 +0530

    deployment config

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
 import { app, server } from './socket/socket.js';
+import path from 'path';
 
 import { syncDb, testDbConnection } from './db/connection.js';
 import { syncPrivateChat } from './models/privateChatModel.js';
@@ -13,6 +14,8 @@ import authRoutes from './routes/authRoutes.js';
 import messageRoute from './routes/messageRoute.js'
 
 const PORT = process.env.PORT || 5000;
+
+const __dirname = path.resolve();
 
 dotenv.config();
 
@@ -26,6 +29,12 @@ app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/message", messageRoute)
+
+app.use(express.static(path.join(__dirname, '/frontend/build')));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '/frontend/build/index.html'));
+});
 
 server.listen(PORT, () => {
     // testDbConnection();
